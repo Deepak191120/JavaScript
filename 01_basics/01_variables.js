cdfc274075e9c642f724f09f74b53914fa917caa
Deepak kumar <78844434+Deepak191120@users.noreply.github.com>
2024-06-20 13:42:33 +0530

    Create 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=23452  // once declared can't be changed because it is constant
+
+let accountEmail="deepak@gmail.com"  // let is a method to initialize variable
+
+var accountPassword="12345" // var is also a way to initialize variable but not used nowdays because of the scope issue
+
+accountCity="Jaipur" // this method is also for variable initializatio, it secure memory space for variabble(not reccommended)
+
+let accountState; // by this we we get the value as undefined
+
+
+
+// accountId=25478           // can't be performed
+accountEmail="hfdf@g,ail.com"
+accountPassword="45123"
+accountCity="Delhi"
+
+
+console.log(accountId)  // by this command we can print the output
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) // by this command we can print the output in table form
